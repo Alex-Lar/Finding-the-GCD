2547d4bd3e11c1dee3be07999d83fef3171b1e9d
Alexander Larin <doremilarin@gmail.com>
2022-02-16 05:31:51 +0300

    rewrote programm

--- script.js
+++ script.js
@@ -1,90 +1,58 @@
 'use strict';
-let inputBtn = document.querySelector('#input-btn'),
-    outputBtn = document.querySelector('#output-btn'),
-    firstMultipliers,
-    secondMultipliers,
-    factIteration = 1;
 
-let firstNum,
-    secondNum,
-    CommDivisor;
+let inputBtn = document.querySelector('#input-btn');
 
 
 inputBtn.addEventListener('click', function () {
-    firstMultipliers = [];
-    secondMultipliers = [];
+    const array = [],
+          dublicOfArr = [];
+    let iterator = 3;
 
-    firstNum = parseInt(prompt('Enter the first number:'));
-    secondNum = parseInt(prompt('Enter the second number:'));
-    if (!parseInt(firstNum) || !parseInt(secondNum)) {
-        alert('Error! Values not entered or entered incorrectly!');
-        while (!parseInt(firstNum) || !parseInt(secondNum)) {
-            firstNum = parseInt(prompt('Enter the first number:'));
-            secondNum = parseInt(prompt('Enter the second number:'));
-        }
+    for (let i = 0; i < iterator; i++) {
+        let userAnswer = parseInt(prompt(`Введите ${i + 1} число`, ''));
+        array.push(userAnswer);
+        dublicOfArr.push(userAnswer);
     }
 
-    factorize(firstNum);
-    factorize(secondNum);
+    let furstNum = array.shift(),
+        secNum = array.shift();
 
-    findGCD(firstMultipliers, secondMultipliers);
+    function findGCD(numOne, numTwo) {
+        let remainder; 
+        while (remainder !== 0) {
+            if (numOne >= numTwo) {
+                if (numOne % numTwo === 0) {
+                    if (array.length === 0) {
+                        return alert(`НОД(${dublicOfArr}) = ${numTwo}`);
+                    } 
+                    else if (array.length !== 0) {
+                        numOne = array.shift();
+                    }
+                } 
+                else if (numOne % numTwo !== 0) {
+                    remainder = numOne % numTwo;
+                    numOne = numTwo;
+                    numTwo = remainder;
 
-    factIteration = 1;
-});
-
-outputBtn.addEventListener('click', function () {
-
-});
-
-
-//Функция разложения числа на множители
-function factorize(factNum) {
-    if (factIteration === 1) {
-        let i = 2;
-        while (factNum !== 1) {
-            if (factNum % i === 0) {
-                factNum /= i;
-                firstMultipliers.push(i);
-            } else if (factNum % i !== 0) {
-                i++;
+                }
+            } 
+            else if (numOne < numTwo) {
+                if (numTwo % numOne === 0) {
+                    if (array.length === 0) {
+                        return alert(`НОД(${dublicOfArr}) = ${numOne}`);
+                    } 
+                    else if (array.length !== 0) {
+                        numTwo = array.shift();
+                    }
+                } 
+                else if (numTwo % numOne !== 0) {
+                    remainder = numTwo % numOne;
+                    numTwo = numOne;
+                    numOne = remainder;
+                }
             }
         }
-        factIteration++;
-        return firstMultipliers;
     }
-    else if (factIteration === 2) {
-        let i = 2;
-        while (factNum !== 1) {
-            if (factNum % i === 0) {
-                factNum /= i;
-                secondMultipliers.push(i);
-            } else if (factNum % i !== 0) {
-                i++;
-            }
-        }
-        return secondMultipliers;
-    }
-}
-
-
-function findGCD(firstArray, secArray) {
-    let prodOfNum = [];
-
-    for (let i = 0; i <= firstArray.length - 1; i++) {
-        if (secArray.includes(firstArray[i])) {
-            prodOfNum.push(firstArray[i]);
-            secArray.splice(secArray.indexOf(firstArray[i]), 1);
-        }
-    }
-
-    if (prodOfNum.length === 0) {
-        return alert('For these numbers there is no GCD.');
-    }
-
-    CommDivisor = prodOfNum.reduce((total, number) => {
-        return total *= number;
-    });
-    alert(`GCD of numbers ${firstNum} and ${secondNum} is ${CommDivisor}`);
-}
-
-
+    findGCD(furstNum, secNum);
+    
+});
